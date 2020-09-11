caf72085b11f8407752c6f966327c1df22be290d
John Bozarov <codinginusa@gmail.com>
2020-09-11 09:07:26 -0400

    Java configuration, config package creates new bean under @Bean annotation

--- src/main/java/com/example/demo/JokeappApplication.java
+++ src/main/java/com/example/demo/JokeappApplication.java
@@ -2,12 +2,15 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
 public class JokeappApplication {
 
-	public static void main(String[] args) {
+	public static void main( String[] args) { 
 		SpringApplication.run(JokeappApplication.class, args);
 	}
+
+	
 
 }
